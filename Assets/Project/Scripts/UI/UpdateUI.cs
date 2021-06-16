05bf4cc3ce7db1c283657e349746f3f4cf59cfd4
waigoma <waichi0302@gmail.com>
2021-06-17 01:37:28 +0900

    create UpdateUI.cs

---
+++ Assets/Project/Scripts/UI/UpdateUI.cs
@@ -0,0 +1,18 @@
+using System.Collections;
+using System.Collections.Generic;
+using Project;
+using UnityEngine;
+
+public class UpdateUI : MonoBehaviour
+{
+    private HpGauge hpGauge;
+    void Start()
+    {
+        hpGauge = new HpGauge();
+    }
+
+    void Update()
+    {
+        
+    }
+}
