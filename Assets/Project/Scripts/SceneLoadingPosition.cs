405b0ac6ab66c2b02e9eae8783734baaf602b351
waigoma <waichi0302@gmail.com>
2021-06-25 12:18:03 +0900

    add SceneLoadingPosition

---
+++ Assets/Project/Scripts/SceneLoadingPosition.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using UnityEngine;
+
+namespace Project
+{
+    public class SceneLoadingPosition : MonoBehaviour
+    {
+        [SerializeField] private SceneMovementData sceneMovementData = null;
+
+        private void Start()
+        {
+            if (sceneMovementData.GetSceneType() == SceneMovementData.SceneType.StartGame)
+            {
+                var initialPosition = GameObject.Find("InitialPosition").transform;
+                transform.position = initialPosition.position;
+                transform.rotation = initialPosition.rotation;
+            }
+            else if (sceneMovementData.GetSceneType() == SceneMovementData.SceneType.FirstRuins)
+            {
+                var initialPosition = GameObject.Find("InitialPosition").transform;
+                transform.position = initialPosition.position;
+                transform.rotation = initialPosition.rotation;      
+            }
+            else if (sceneMovementData.GetSceneType() == SceneMovementData.SceneType.FirstRuinsToVillage)
+            {
+                var initialPosition = GameObject.Find("InitialPositionFirstRuinsToFirstVillage").transform;
+                transform.position = initialPosition.position;
+                transform.rotation = initialPosition.rotation;
+            }
+        }
+    }
+}
