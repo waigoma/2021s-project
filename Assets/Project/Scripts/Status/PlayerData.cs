8c9d4a4902d6236598757aabd80dae8ed8d1c54b
waigoma <waichi0302@gmail.com>
2021-07-09 11:36:11 +0900

    PlayerDataを扱うクラス作成

---
+++ Assets/Project/Scripts/Status/PlayerData.cs
@@ -0,0 +1,35 @@
+using UnityEngine;
+
+namespace Project
+{
+    public class PlayerData : MonoBehaviour
+    {
+        [SerializeField]private PlayerStatus playerStatus;
+
+        private HpGauge hpGauge;
+
+        private void Start()
+        {
+            hpGauge = GameObject.Find("UpdateUI").GetComponent<HpGauge>();
+        }
+
+        void Update()
+        {
+            if (Input.GetButtonDown("Fire1"))
+            {
+                OnDamage(10);
+                Debug.Log($"{playerStatus.CharacterName} : {playerStatus.Hp}");
+            }
+            else if (Input.GetButtonDown("Fire2"))
+            {
+                Debug.Log($"{playerStatus.CharacterName} : {playerStatus.Hp}");
+            }
+        }
+
+        void OnDamage(int dmg)
+        {
+            hpGauge.GaugeReduction(dmg);
+            playerStatus.Hp -= dmg;
+        }
+    }
+}
