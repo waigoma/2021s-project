d367a19d613011b0a28fa1c1bd2f61123f08be17
waigoma <waichi0302@gmail.com>
2021-08-08 23:19:22 +0900

    剣がモブに当たるとモブがよろける

---
+++ Assets/Project/Scripts/Status/MobStatus.cs
@@ -0,0 +1,83 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using UnityEngine;
+
+namespace Project
+{
+    [Serializable]
+    [CreateAssetMenu(fileName = "PlayerStatus", menuName = "CreatePlayerStatus")]
+    public class PlayerStatus : BaseStatus
+    {
+        [SerializeField] private int earnedExp;
+        [SerializeField] private int experience;
+        [SerializeField] private int reqExp;
+        [SerializeField] private int stamina;
+        [SerializeField] private Item equipWeapon;
+        [SerializeField] private Item equipArmor;
+
+        [SerializeField] private ItemDictionary itemDictionary;
+        
+
+        public int Experience
+        {
+            get => experience;
+            set => experience = value;
+        }
+
+        public int ReqExp
+        {
+            get => reqExp;
+            set => reqExp = value;
+        }
+        
+        public int Stamina
+        {
+            get => stamina; 
+            set => stamina = value;
+        }
+
+        public ItemDictionary GetItemDictionary()
+        {
+            return itemDictionary;
+        }
+
+        public IOrderedEnumerable<KeyValuePair<Item, int>> GetSortItemDictionary()
+        {
+            return itemDictionary.OrderBy(item => item.Key.HiraganaName);
+        }
+        
+        public int SetItemNum(Item tmpItem, int num)
+        {
+            return itemDictionary[tmpItem] = num;
+        }
+
+        public int GetItemNum(Item item)
+        {
+            return itemDictionary[item];
+        }
+
+        public void AddExp(int exp)
+        {
+            Experience += exp;
+            earnedExp += exp;
+            LevelUp();
+        }
+
+        public void LevelUp()
+        {
+            // 経験値が必要経験値を超えなければ終了
+            if (Experience <= ReqExp) return;
+            
+            // レベル +1
+            Level++;
+            // 経験値レベルアップ計算
+            Experience -= ReqExp;
+            // 必要経験値設定
+            ReqExp = Level * 10;
+            // 経験値リセット
+            Experience = 0;
+        }
+    }
+
+}
